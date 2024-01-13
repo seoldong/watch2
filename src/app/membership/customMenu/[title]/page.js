1e6a2cfd44790a24fdeae616f045ad436ebb126f
seolDong <dh10513@gmail.com>
2024-01-13 22:42:41 +0900

    Type error correction.2

--- src/app/membership/customMenu/[title]/page.js
+++ src/app/membership/customMenu/[title]/page.js
@@ -13,9 +13,8 @@ function CustomPage() {
   const params = useParams();
   const title = params.title;
 
-  const [settingTime, setSettingTime] = useState();
+  const [settingTime, setSettingTime] = useState(null);
   const [settingBtn, setSettingBtn] = useState(true);
-  console.log('settingTime', settingTime, typeof settingTime);
 
 
   useEffect(() => {
